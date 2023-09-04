2d0498c4ac8b4e8d31b3665fa21b894a3b619c98
dieguxo91 <diegogarcia0512@gmail.com>
2023-09-04 13:51:07 +0200

    modulos finalizados

---
+++ 13_modulos.py
@@ -0,0 +1,12 @@
+# Modulos
+
+from module import said_Hello # por si solo queremos importar una función en concreto
+
+import module # no acepta numeros en el nombre del modulo
+import math # podriamos importar librerias existentes como en otros lenguajes
+
+module.sum(3,2)
+
+module.said_Hello()
+
+print(math.floor(4.2))
---
+++ module.py
@@ -0,0 +1,7 @@
+# Prueba para el modulo
+
+def sum(number_one, number_two):
+    print(number_one + number_two)
+
+def said_Hello():
+    print("Holiiii")
