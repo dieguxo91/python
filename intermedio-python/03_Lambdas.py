d45a084a37eabbe100320bbeffdcc26e9dd7b8d9
dieguxo91 <diegogarcia0512@gmail.com>
2023-09-05 10:03:42 +0200

    lambdas finalizado

---
+++ intermedio-python/03_Lambdas.py
@@ -0,0 +1,14 @@
+# lambda
+
+# funciones en una variable que luego la podemos usar como una funcion
+sum_two_values = lambda first_value, second_value : first_value + second_value
+
+print(sum_two_values(2,4))
+
+print_value = lambda value : print(value)
+
+print_value(5)
+
+multiples_values = lambda first_value, second_value : first_value * second_value 
+
+print(multiples_values(5, 6))
